7fac00f0f1046330189b97c4165c62b7109e5d8e
u7861723 <u7861723@anu.edu.au>
2025-05-08 15:46:56 +1000

    22

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ Session(app)
 CLIENT_ID = os.getenv("CLIENT_ID")
 CLIENT_SECRET = os.getenv("CLIENT_SECRET")
 TENANT_ID = os.getenv("TENANT_ID")
-REDIRECT_URI = "http://localhost:5000/callback"
+REDIRECT_URI = "https://test-api-aht9.onrender.com/callback"
 AUTHORITY = f"https://login.microsoftonline.com/{TENANT_ID}"
 SCOPE = "openid profile email OnlineMeetings.Read OnlineMeetingTranscript.Read.All Calendars.Read User.Read"
 
