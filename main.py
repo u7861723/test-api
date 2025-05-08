3d43bded992814d8a6877fd3bf2eb329d2179d03
u7861723 <u7861723@anu.edu.au>
2025-05-08 15:38:05 +1000

    22

--- main.py
+++ main.py
@@ -106,5 +106,9 @@ def meetings():
     output += "</ul>"
     return output
 
+import os
+
 if __name__ == "__main__":
-    app.run(port=5000, debug=True, use_reloader=False)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
